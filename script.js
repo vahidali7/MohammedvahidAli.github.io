52eafd22542567d3f3dfe9e3e4366120d1044d15
Mohammed Vahid Ali <122780372+vahidali7@users.noreply.github.com>
2023-12-03 19:57:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -62,3 +62,41 @@ const typed = new Typed ('.multiple-text', {
 })
 
 
+
+const form = document.querySelector("form");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("number");
+const subject = document.getElementById("sub");
+const message = document.getElementById("message");
+
+function sendMail() {
+    // Check if any of the fields is empty
+    if (!name.value || !email.value || !phone.value || !subject.value || !message.value) {
+        alert("Please fill in all fields.");
+        return; // Stop the function if any field is empty
+    }
+
+    const bodyMessage = `Name: ${name.value}<br> Email: ${email.value}<br> Phone Number:  ${phone.value}<br> Subject: ${subject.value}<br> Message: ${message.value}`;
+
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "bemuslim26@gmail.com",
+        Password: "FC6DB57C5011662FB6D554414A7668A51338",
+        To: 'bemuslim26@gmail.com',
+        From: "bemuslim26@gmail.com",
+        Subject: subject.value,
+        Body: bodyMessage
+    }).then(
+        message => {
+            if (message === "OK") {
+                alert("Message sent successfully!");
+                form.reset();
+            }
+        }
+    );
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+});
